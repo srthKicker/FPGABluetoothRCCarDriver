919330608a40a15b9d340904b39b7b6ccde2fb0a
srthKiker <mesckewmcevs@yahoo.com>
2026-04-25 12:53:44 -0600

    Ported bit reading

--- FPGARCCar.srcs/sources_1/new/rxBit.vhd
+++ FPGARCCar.srcs/sources_1/new/rxBit.vhd
@@ -1,23 +1,36 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-
-entity rxBit is --Sim
-  Port ( baudClk,--Clock that runs at the baud rate of UART (9600)
-   inputBit,  --Directly tied to the I/O pin connected to the HC-06 BT receiver
-   rxEnable,  --Simple enable (don't recieve if not actively transmitting)
-   rst: --Restarts index to 0;
-   in std_logic; 
+--Simply reads a bit from UART and writes it to data
+--baudClk will be timed so that its rising edge
+--is around the center of the UART pulse to use stable values
+--in calculations
+entity rxBit is
+  Port ( 
+   baudClk: in std_logic;--Clock that runs at the baud rate of UART (9600)
+   inputBit: in std_logic; --Directly tied to the I/O pin connected to the HC-06 BT receiver
+   rxEnable: in std_logic;  --Simple enable (don't recieve if not actively transmitting)
+   rst: in std_logic; --Restarts index to 0, active high
    
-   triggerFSM: out std_logic; --If we just recieved last bit in transmit byte, update motors
-   --triggerFSM will be an input of our ctrlMotor entity
+   triggerFSM: out boolean; --triggerFSM will be an input of our ctrlMotor entity
    receivedByte: out std_logic_vector(7 downto 0) --7 is MSB, 0 is LSB
    );
 end rxBit;
 
 architecture Behavioral of rxBit is
-
+    signal index : integer range 0 to 7 := 0;
 begin
 
+process(baudClk, rst) begin
+    if rst='1' then
+        index <= 0;
+        triggerFSM <= false;
+        receivedByte <= (others => 'U');
+    elsif rising_edge(baudClk) then
+        triggerFSM <= (index = 7); --When we're done rxing, update motors
+        receivedByte(index) <= inputBit;
+        index <= index + 1;
+    end if;
+end process;
 
 end Behavioral;
