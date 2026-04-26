42727bae86316614f6117e58fb7aa19d02fe906c
srthKiker <mesckewmcevs@yahoo.com>
2026-04-25 18:32:04 -0600

    Improved PWM driver efficiency

--- FPGARCCar.srcs/sources_1/new/pwmGenerator.vhd
+++ FPGARCCar.srcs/sources_1/new/pwmGenerator.vhd
@@ -2,6 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
+--Drives 3 bit resolution PWM to the output "driver".
+--Four of these are instantiated in ctrlMotor
+
 entity pwmGenerator is
     Port ( 
         systemClock : in std_logic; --used to time
@@ -17,40 +20,43 @@ architecture pwmGenerator_arch of pwmGenerator is
     constant CLOCK_FREQ : integer := 100000000; --internal clock freq, 100MHz
     --How many clock cycles before we restart PWM calculations, 5000 for 20khz and 100Mhz
     constant PWM_PERIOD_CYCLES : integer range 0 to 16384 := CLOCK_FREQ/PWM_FREQUENCY;
-    constant PWM_PERIOD_SCALED: integer range 0 to 8192 := PWM_PERIOD_CYCLES/7;
+    constant PWM_PERIOD_SCALED: integer range 0 to 8192 := PWM_PERIOD_CYCLES/7; --714
+    
+    --Change to 1 if off by 1 matters, 0 if it doesnt
+    constant CLOCK_START : integer range 0 to 1 := 1;
+    
     --Signals to do with counting clock cycles and PWM 
-    signal clockCount : integer range 0 to 16384 := 1;--1 to remove off by 1 error
+    signal clockCount : integer range 0 to 16384 := CLOCK_START;
     --will be PWM_PERIOD_CYCLES * (
     signal numHighCycles: integer range 0 to 16384 := 0; --how wide the pulse will be
-    signal completedHighCycles: integer range 0 to 16384 := 0; --how wide the pulse has been
 begin
     process(systemClock, globalReset) begin
+    
          if (globalReset = '1') then --on reset handle signals and driver output
-            --recalculate pwm duty
-            numHighCycles <= (PWM_PERIOD_SCALED) * to_integer(unsigned(dutyCycle));
-            completedHighCycles <= 0;
-            clockCount <= 1;
-            --turn motor off
-            driver <= '0';
-        elsif rising_edge(systemClock) then
+            numHighCycles <= 0;
+            clockCount <= CLOCK_START;
+            driver <= '0'; --turn off motor
+            
+        elsif rising_edge(systemClock) then --On every clock edge
+            --Count clock cycles
             if (clockCount = PWM_PERIOD_CYCLES) then --if we are done with this period
-                completedHighCycles <= 0;--reset high cycle count
-                clockCount <= 0; --reset clock cycle count
-            else --Normally, we do this
-                --Calculate how many high cycles we need to do
+                --Calculate how many high cycles we need to do next cycle
                 --3 bit resolution duty means 7 should max the num of cycles
                 --Again, non blocking means a 1 clock cycle delay, but thats okay
                 numHighCycles <= (PWM_PERIOD_SCALED) * to_integer(unsigned(dutyCycle)); 
+                clockCount <= CLOCK_START; --reset clock cycle count
+            else --If we are midway through a PWM period
                 clockCount <= clockCount + 1; --increment clock count always
-                
-                if (completedHighCycles < numHighCycles) then
-                    driver <= '1';
-                    completedHighCycles <= completedHighCycles + 1; --increment
-                else
-                    driver <= '0';
-                end if;
+            end if; --end clock counting
+            
+            --Motor driving logic
+            --This logic works for all clock cycles regardless of where in the cycle we are
+            if (clockCount < numHighCycles) then --if the pulse needs to be wider still
+                driver <= '1';
+            else --if the pulse is done being high
+                driver <= '0';
             end if;
         end if;
     end process;
-
+    
 end pwmGenerator_arch;
